9ac7fa339d53e451561e856b75412990c34b7ac3
MariySemenenko <mashyni92@gmail.com>
2023-04-24 08:37:56 +0300

    markup

--- src/PixabayAPI.js
+++ src/PixabayAPI.js
@@ -6,14 +6,16 @@ axios.defaults.baseURL = 'https://pixabay.com/api/';
 // axios.defaults.headers.common['Authorization'] = 'Client-ID 35692363 -35692363-708937b69116888a5fb6ef1bd';
 const API_KEY = '35692363-708937b69116888a5fb6ef1bd';
   //використовую клас PixabayAPI для отримання нанних з сайту Pixabay
-export class PixabayAPI {
+export class PixabayAPI { //приватні властивості класу
     #page = 1;
     #per_page = 40;
     #query = '';
     #totalPages = 0;
 
+
+    // повертає дані з API сайту Pixabay
     async getPhotos() {
-        const params = {
+        const params = { //params: містить параметри запиту
             page: this.#page,
             q: this.#query,
             per_page: this.#per_page,
@@ -26,8 +28,8 @@ export class PixabayAPI {
         const urlAXIOS = `?key=${API_KEY}`;
         
 
-        const { data } = await axios.get(urlAXIOS, { params, });
-        return data;
+        const { data } = await axios.get(urlAXIOS, { params, });//передаю params у axios.get
+        return data; //повертаю дані відповіді в форматі JSON
     }
 
     get query() {
